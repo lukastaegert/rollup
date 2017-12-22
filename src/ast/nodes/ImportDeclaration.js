6e4d02785e554057c959551ce76e0bdab1398b4e
Lukas Taegert <lukastaegert@users.noreply.github.com>
2017-12-22 10:49:08 +0100

    Update ImportDeclaration.js
    Again I did non try this but it should actually work in all cases, i.e.
    * `import x, {y, z} from './x.js'`
    * `import * as x from './x.js'`

--- src/ast/nodes/ImportDeclaration.js
+++ src/ast/nodes/ImportDeclaration.js
@@ -8,8 +8,16 @@ export default class ImportDeclaration extends Node {
 	}
 
 	render ( code ) {
-		if (!this.included) {
-			code.remove( this.start, this.next || this.end );
+		const excludedSpecifiers = this.specifiers.map(specifier => !this.scope.findVariable(specifier.local.name).included);
+		if (excludedSpecifiers.every(Boolean) {
+			code.remove( this.start, this.end ); // None of the imported variables is used => remove whole statement
+		} else {
+			this.specifiers.forEach((specifier, position) => {
+				if (excludedSpecifiers[position]) {
+					const specifierEnd = position === this.specifiers.length - 1 ? specifier.end : this.specifiers[position + 1].start;
+					code.remove( this.start, specifierEnd ); // Remove this specifier + everything up to the next specifier
+				}
+			})
 		}
 	}
 }
